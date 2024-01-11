797e7c9507900a28864bc335b093bfff15dc203d
AleksandraLos <oluchna@hotmail.com>
2024-01-11 20:14:46 +0100

    added form to signup component

--- snapgram/src/_auth/AuthLayout.tsx
+++ snapgram/src/_auth/AuthLayout.tsx
@@ -1,25 +1,25 @@
 import { Outlet, Navigate } from "react-router-dom";
-
+import SideImage from '../assets/side-img.svg'
 
 export default function AuthLayout() {
   const  isAuthenticated = false;
-
   return (
     <>
-      {isAuthenticated ? (
-        <Navigate to="/" />
-      ) : (
-        <>
-          <section className="flex flex-1 justify-center items-center flex-col py-10">
-            <Outlet />
-          </section>
-          <img
-            src="/assets/side-img.svg"
-            alt="logo"
-            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
-          />
-        </>
-      )}
-    </>
+    {isAuthenticated ? (
+      <Navigate to="/" />
+    ) : (
+      <>
+        <section className="flex flex-1 justify-center items-center flex-col py-10">
+          <Outlet />
+        </section>
+         <img
+          src={SideImage}
+          alt=""
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+        />
+      </>
+    )}
+  </>
+
   );
 }
--- snapgram/src/_auth/forms/SignupForm.tsx
+++ snapgram/src/_auth/forms/SignupForm.tsx
@@ -1,23 +1,105 @@
-import { Navigate, Outlet } from "react-router-dom"
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+
+import { Button } from "@/components/ui/button"
+import { useForm } from 'react-hook-form'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { SiguUpValidationSchema } from "@/lib/validation"
+import Logoipsum from '../../../src/assets/logoipsum-289.svg'
 
 export const SignupForm = () => {
-  const  isAuthenticated = false;
+
+  const isLoading = true;
+   // 1. Define your form.
+   const form = useForm<z.infer<typeof SiguUpValidationSchema>>({
+    resolver: zodResolver(SiguUpValidationSchema),
+    defaultValues: {
+      name:"",
+      username: "",
+      email:"",
+      password:""
+    },
+  })
+ 
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof SiguUpValidationSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
   return (
-    <>
-    {isAuthenticated ? (
-      <Navigate to="/" />
-    ) : (
-      <>
-        <section className="flex flex-1 justify-center items-center flex-col py-10">
-          <Outlet />
-        </section>
-        <img
-          src="/assets/side-img.svg"
-          alt="logo"
-          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
-        />
-      </>
-    )}
-  </>
+    <Form {...form}>
+      <div className="sm:w-420 flex-center flex-col"><img src={Logoipsum} alt="Logo" /><h2 className="h3-bold md:h2-bold pt-5 sm:pt12">Create a new accout</h2><p className="text-light-3 small-medium md:base-regular">To use snapgram entre your account details.</p>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+      <FormField
+        control={form.control}
+        name="name"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Name :</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="Enter your name" className="shad-input"{...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="username"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Username :</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="Enter your username" className="shad-input"{...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="email"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Email :</FormLabel>
+            <FormControl>
+              <Input type="text" placeholder="Enter your email" className="shad-input"{...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          
+        )}
+      />
+        <FormField
+        control={form.control}
+        name="password"
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>Password: :</FormLabel>
+            <FormControl>
+              <Input type="password" placeholder="Enter your password" className="shad-input"{...field} />
+            </FormControl>
+            <FormMessage />
+          </FormItem>
+          
+        )}
+      />
+      <Button type="submit" className="shad-button_primary">{isLoading ? (<div className="flex-center gap-2">Loading . . .</div>): "Sign up"}</Button>
+    </form>
+    </div>
+  </Form>
+  
+  
   )
 }
